675c111c8fc9efd4ba4c91fe6c3dd24edc434cc4
Richard Joe <rjoe19@hotmail.com>
2016-08-24 15:51:06 +1200

    setup public folder for serving static files

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function(req,res){
+  res.send('Hello World!')
+});
+
+app.use('/public', express.static('public'));
+
+var port = Number(process.env.PORT || 5000);
+app.listen(port);
