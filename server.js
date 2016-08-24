e3cc0d1532041224c2ea355835ed0d29713a8442
Richard Joe <rjoe19@hotmail.com>
2016-08-24 16:48:13 +1200

    setup views engine, handlebars templates and dynamic views?

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 var express = require('express');
-var app = express();
+var app = express(); // setup instance of express
+var exphbs = require('express-handlebars');
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));  // set view engine to handlebars
+app.set('view engine', 'handlebars');
 
 app.get('/', function(req,res){
-  res.send('Hello World!')
+  var luckyNumber = Math.round(Math.random() * 10);
+  res.render('index',
+      {luckyNumber: luckyNumber});  // create dynamic views, luckyNumber changes  randomly
+});
+
+app.get('/about', function(req,res){
+  res.render('about');  // route will by default serve files in public folder
 });
 
 app.use('/public', express.static('public'));
